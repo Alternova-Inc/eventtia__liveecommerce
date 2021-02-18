21a5ba08fae12dd758255337dda4c08871fb14f5
Andrea Rojas <andrea.rojas@alternova.io>
2021-02-18 17:58:55 -0500

    (wip) Create widgets

--- app.js
+++ app.js
@@ -1,12 +1,182 @@
-let div = document.createElement("div");
 let params = document.getElementById("eventtia-library").src;
 
-//set modal url from src
-let url= '';
-if (params.indexOf('url') !== -1) {
-  let count = params.indexOf('url');
-  url = params.slice(count + 4);
+// create months and week days in french
+const french_months = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Décembre"];
+const french_week_days = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+//set 'inline' attribute to a script tag (I'm not sure of that, is a test)
+scriptStyle = document.getElementById("eventtia-library").setAttribute('style', 'display: inline;');
+
+//Split by ? and then split by ?
+let params_array = params.split("?");
+const elements_array = [];
+
+for (let i = 0; i < params_array.length; i++) {
+    elements_array.push(params_array[i].split("="));
 }
+
+// read data from url
+let url = elements_array[1][1];
+let imageUrl = elements_array[2][1];
+let widgetType = elements_array[3][1];
+let event_utc_date = elements_array[4][1];
+let event_utc_time = elements_array[5][1];
+let event_duration_hours = elements_array[6][1];
+
+// split the date and time so we can create a new date using JS
+let event_utc_date_array = event_utc_date.split("-");
+let event_utc_time_array = event_utc_time.split("-");
+
+// Create new date using JS
+let event_utc_js_datetime = new Date(parseInt(event_utc_date_array[0]), parseInt(event_utc_date_array[1]) - 1, parseInt(event_utc_date_array[2]), parseInt(event_utc_time_array[0]), parseInt(event_utc_time_array[1]), 0, 0);
+let event_time_user_adjusted = new Date(parseInt(event_utc_date_array[0]), parseInt(event_utc_date_array[1]) - 1, parseInt(event_utc_date_array[2]), parseInt(event_utc_time_array[0]), parseInt(event_utc_time_array[1]), 0, 0);
+let event_time_finished = new Date(parseInt(event_utc_date_array[0]), parseInt(event_utc_date_array[1]) - 1, parseInt(event_utc_date_array[2]), parseInt(event_utc_time_array[0]), parseInt(event_utc_time_array[1]), 0, 0);
+
+// Find local time and its difference with UTC
+let current_time = new Date();
+let time_offset = current_time.getTimezoneOffset() / -60;
+
+// Find event time adjusted to the user timezone.
+event_time_user_adjusted.setHours(event_time_user_adjusted.getHours() + time_offset);
+
+// Find event finished time
+event_time_finished.setHours(event_time_finished.getHours() + (time_offset + parseInt(event_duration_hours)));
+
+// Create human readable date in french
+//event_utc_js_datetime.setHours(event_utc_js_datetime.getHours() - time_offset);
+//event_utc_js_datetime.setHours(event_utc_js_datetime.getHours() - event_duration_hours);
+
+// Find if event Scheduled, Live, Rediffusion
+let event_status = "Commencera bientôt";
+function check_event_status() {
+  // console.log("Check event status called");
+  current_time = new Date();
+  if (current_time < event_time_user_adjusted) {
+    // event is Shceduled
+    event_status = "Commencera bientôt";
+  } else if (current_time > event_time_finished) {
+    // event is over
+    event_status = "Rediffusion";
+  } else {
+    //event is live
+    event_status = "Live";  
+  }
+
+  if (widgetType == "compact") {
+    // console.log("Update compact card", event_status);
+    document.getElementsByClassName("eventtia-card-compact_badge")[0].innerHTML = "" + event_status + "\n";
+  } else if (widgetType == "expand") {
+    // console.log("Update expand card", event_status);
+    document.getElementsByClassName("eventtia-card-expand_badge")[0].innerHTML = "" + event_status + "\n";
+  } else {
+    console.log("Unsupported widget type.");
+  }
+  // console.log("bye");
+}
+
+// Log all variables for testing.
+// console.log("modalUrl", url);
+// console.log("imageUrl", imageUrl);
+// console.log("widgetType", widgetType);
+// console.log("event_utc_date", event_utc_date);
+// console.log("event_utc_time", event_utc_time);
+// console.log("event_duration_hours", event_duration_hours);
+// console.log("current_time", current_time);
+// console.log("time_offset", time_offset);
+// console.log("-----------------------------");
+// console.log("event_utc_js_datetime", event_utc_js_datetime);
+// console.log("event_time_user_adjusted", event_time_user_adjusted);
+// console.log("event_time_finished", event_time_finished);
+// console.log("event_status", event_status);
+
+//Add a card when is a compact type
+if (widgetType == 'compact') {
+  
+  //get button text
+  let button = document.getElementsByClassName("eventtia-btn")[0];
+  let btn_text = button.textContent;
+
+  //delete button element
+  let node = button.parentNode;
+  node.removeChild(button);
+
+  // create div card compact
+  let card = document.createElement("div");
+  card.setAttribute('style', 'display: inline;');
+  card.innerHTML =
+    '<ul class="eventtia-cards-compact">\n' +
+          '<li class="eventtia-cards-compact_item eventtia-btn">\n' +
+            '<div class="eventtia-card-compact">\n' +
+              '<div class="eventtia-card-compact_image">\n' +
+                  '<img class="eventtia-card-compact_img" src="' + imageUrl + '">\n' +
+              '</div>\n' +
+              '<div class="eventtia-card-compact_content">\n' +
+                  '<p class="eventtia-card-compact_badge">\n' +
+                      event_status + '\n' +
+                  '</p>\n' +
+                  '<h2 class="eventtia-card-compact_title">\n' +
+                    btn_text +'\n' +
+                  '</h2>\n' +
+              '</div>\n' +
+            '</div>\n' +
+          '</li>\n' +
+      '</ul>\n';
+  document.body.appendChild(card);
+}
+
+//Add a card when is a compact type
+if (widgetType == 'expand') {
+  
+  //get content for card expand
+  let card_title = document.getElementsByClassName("eventtia-title")[0].textContent;
+  let card_description = document.getElementsByClassName("eventtia-description")[0].textContent;
+  let btn_text = document.getElementsByClassName("eventtia-btn")[0].textContent;
+
+  //delete button element
+  let card_content = document.getElementsByClassName("eventtia-card-expand-content")[0];
+  while (card_content.firstChild) {
+    card_content.removeChild(card_content.firstChild);
+  }
+
+  // create div card expnand
+  let card = document.createElement("div");
+  card.innerHTML =
+      '<ul class="eventtia-cards-expand">\n' +
+        '<li class="eventtia-cards-expand_item">\n' +
+          '<div class="eventtia-card-expand">\n' +
+            '<div class="eventtia-card-expand_image">\n' +
+                '<img class="eventtia-card-expand_img" src="' + imageUrl + '">\n' +
+            '</div>\n' +
+            '<div class="eventtia-card-expand_content">\n' +
+                '<p class="eventtia-card-expand_badge">\n' +
+                    event_status + '\n' +
+                '</p>\n' +
+                '<h5 class="eventtia-card-expand_date">\n' +
+                    event_time_user_adjusted + '\n' +
+                '</h5>\n' +
+                '<h2 class="eventtia-card-expand_title">\n' +
+                    card_title + '\n' +
+                '</h2>\n' +
+                '<p class="eventtia-card-expand_text">\n' +
+                    card_description + '\n' +
+                '</p>\n' +
+                '<div class="eventtia-card-expand_button">\n' +
+                    '<button class="eventtia-card-expand_btn eventtia-btn">\n' +
+                        btn_text +'\n' +
+                    '</button>\n' +
+                '</div>\n' +
+            '</div>\n' +
+          '</div>\n' +
+        '</li>\n' +
+    '</ul>\n';
+  document.body.appendChild(card);
+}
+
+// Run Check
+check_event_status();
+
+// Check event status every minute
+let status_interval = setInterval(function(){ check_event_status(); }, 60000);
 
 //connect to supabase and define globals
 const { createClient } = supabase;
@@ -14,6 +184,7 @@ supabase = createClient('https://grjotsrqxlcjdhqqjmai.supabase.co', 'eyJhbGciOiJ
 let db_id = "none"
 let user_id = "8baf6150-aee1-43eb-8bdb-451c723dbf21"; //hardcoded just to generate a new UUID.
 let interval = null;
+let current_utc_time = null;
 
 //Generate unique UUID gor this user
 function gen_uuid() {
@@ -29,6 +200,7 @@ function gen_uuid() {
         // Write successfull
         // console.log(data);
         db_id = data[0].id;
+        current_utc_time = data[0].created_at
 
         //save to localstorage
         localStorage.setItem("eventtia-tag", db_id);
@@ -75,6 +247,7 @@ function update_session(type) {
   }
 }
 
+let div = document.createElement("div");
 div.innerHTML =
   '<div class="modal-embed">\n' +
   '<div class="modal-content">\n' +
@@ -116,20 +289,10 @@ modal.setAttribute("style", "display: none; position: fixed; z-index: 9999; left
 modalContent.setAttribute("style", "background-color: #fefefe; margin: auto; padding: 0px; border: 1px solid #888;");
 embed.setAttribute("style", "height: calc(100vh - 55px);");
 
-//set image url from src
-let imageUrl= '';
-if (params.indexOf('img') !== -1) {
-  count = params.indexOf('img');
-  imageUrl = params.slice(count + 4,  params.indexOf('type') - 1);
-}
+var stylesheet = document.createElement("link");
 
-//set widget type from src
-let widgetType= '';
-if (params.indexOf('type') !== -1) {
-  count = params.indexOf('type');
-  widgetType = params.slice(count + 5);
-}
+stylesheet.type = "text/css";
+stylesheet.rel = "stylesheet";
+stylesheet.href = "style-widgets.css";
 
-console.log("modalUrl", url);
-console.log("imageUrl",imageUrl);
-console.log("widgetType",widgetType);
+document.body.appendChild(stylesheet);
