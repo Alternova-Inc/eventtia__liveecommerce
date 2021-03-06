356836a574b848a0994149143140864bc634b46a
ricardo vleasquez <ricardo@alternova.co>
2021-03-05 22:02:30 -0500

    refactor code to fix problems with the update of the widgets

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let params = document.getElementById("eventtia-library").src;
 
 // create months and week days in french
 const french_months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"];
-const french_week_days = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+const french_week_days = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
 
 //set 'inline' attribute to a script tag (I'm not sure of that, is a test)
 scriptStyle = document.getElementById("eventtia-library").setAttribute('style', 'display: inline;');
@@ -40,7 +40,7 @@ if (env === 'prod') {
 supabase = createClient(host, key);
 let db_id = "none"
 let user_id = "8baf6150-aee1-43eb-8bdb-451c723dbf21"; //hardcoded just to generate a new UUID.
-let interval = null;
+let intervals_obj = {};
 let current_utc_time = null;
 
 //Get all public events
@@ -97,7 +97,7 @@ function getParsedDates(event_utc_datetime, event_duration_minutes) {
     minutes = "0" + minutes;
   }
 
-  let week_day = event_utc_js_datetime.getDay();
+  let week_day = event_time_user_adjusted.getDay();
   let readable_date = "" + french_week_days[week_day] + " " + day + " " + french_months[month];
   readable_date += " " + year + ", " + hours + ":" + minutes;
 
@@ -140,22 +140,22 @@ if (localStorage.getItem("eventtia-tag") === null) {
 }
 
 // checks event status based on dates
-function check_event_status(event_time_finished, event_time_user_adjusted) {
+function check_event_status(event_time_finished, event_time_user_adjusted, id_eventtia, widget_type, first_call) {
   let event_status = "Commencera bientôt";
   let status_class_add = "scheduled-status";
   let status_class_remove = "scheduled-status";
+  let card = null;
 
-  // Check current status
-  if (event_status == "Commencera bientôt") {
-    status_class_remove = "scheduled-status";
-  } else if (event_status == "Rediffusion") {
-    status_class_remove = "replay-status";
-  } else if (event_status == "Live") {
-    status_class_remove = "live-status";
-  } else {
-    console.log("Status not supported.");
+  if (!first_call) {
+    card = document.getElementById(id_eventtia);
+    // Get current status
+    if (widget_type === "expand") {
+      status_class_remove = card.getElementsByClassName("eventtia-card-expand_badge")[0].classList[1];
+    } else if (widget_type === "compact") {
+      status_class_remove = card.getElementsByClassName("eventtia-card-compact_badge")[0].classList[1];
+    }
   }
-
+  
   let current_time = new Date();
   if (current_time < event_time_user_adjusted) {
     // event is Shceduled
@@ -170,6 +170,7 @@ function check_event_status(event_time_finished, event_time_user_adjusted) {
     event_status = "Live";
     status_class_add = "live-status";
   }
+
 
   let return_obj = {
     "event_status": event_status,
@@ -199,68 +200,66 @@ function organizeDataFromSupabase(container_data) {
   return return_obj;
 }
 
+function editCardClasses(card, badge_class, btn_class, obj_event_status) {
+  // console.log("Update compact card", event_status);
+  let card_badge = card.getElementsByClassName(badge_class)[0];
+  let card_btn = card.getElementsByClassName(btn_class)[0];
+
+  card_badge.innerHTML = "" + obj_event_status["event_status"] + "\n";
+
+  // update badge color
+  card_badge.classList.remove(obj_event_status["status_class_remove"]);
+  card_badge.classList.add(obj_event_status["status_class_add"]);
+  
+  // update button color
+  card_btn.classList.remove(obj_event_status["status_class_remove"]);
+  card_btn.classList.add(obj_event_status["status_class_add"]);
+}
+
 // function that modifies the DOM
-function updateLayoutFromData(data) {
-  const events_data = data.events_data;
-  const parsed_dates = data.parsed_dates;  
-  let event_status_obj = {};
-  let event_key = ""
+function updateLayoutFromData(obj_data, obj_dates, obj_event_status, first_call) {
+  // We check the widget type to add html
+  if (obj_data.widget_type === "expand") {
+    let card = document.getElementById(obj_data.id);
 
-  for (let i = 0; i < event_ids.length; i++) {
-    event_key = event_ids[i].id;
-
-    // Find if event Scheduled, Live, Rediffusion
-    event_status_obj = check_event_status(parsed_dates[event_key]["event_time_finished"], parsed_dates[event_key]["event_time_user_adjusted"]);
-
-    // We check the widget type to add html
-    if (events_data[event_key].widget_type === "expand") {
-      let card = document.getElementById(event_key);
-
+    if (first_call) {
       // create div card expand
       card.innerHTML =
-        '<ul class="eventtia-cards-expand">\n' +
-        '<li class="eventtia-cards-expand_item">\n' +
-        '<div class="eventtia-card-expand">\n' +
-        '<div class="eventtia-card-expand_image">\n' +
-        '<img class="eventtia-card-expand_img" src="' + events_data[event_key].widget_image + '">\n' +
-        '</div>\n' +
-        '<div class="eventtia-card-expand_content">\n' +
-        '<p class="eventtia-card-expand_badge">\n' +
-          event_status_obj["event_status"] + '\n' +
-        '</p>\n' +
-        '<h5 class="eventtia-card-expand_date">\n' +
-          parsed_dates[event_key]["readable_date"] + '\n' +
-        '</h5>\n' +
-        '<h2 class="eventtia-card-expand_title">\n' +
-          events_data[event_key].widget_title_text + '\n' +
-        '</h2>\n' +
-        '<p class="eventtia-card-expand_text">\n' +
-          events_data[event_key].widget_description_text + '\n' +
-        '</p>\n' +
-        '<div class="eventtia-card-expand_button">\n' +
-        '<button class="eventtia-card-expand_btn eventtia-btn" eventtia-id="' + event_key + '">\n' +
-          events_data[event_key].widget_button_text + '\n' +
-        '</button>\n' +
-        '</div>\n' +
-        '</div>\n' +
-        '</div>\n' +
-        '</li>\n' +
-        '</ul>\n';
+      '<ul class="eventtia-cards-expand">\n' +
+      '<li class="eventtia-cards-expand_item">\n' +
+      '<div class="eventtia-card-expand">\n' +
+      '<div class="eventtia-card-expand_image">\n' +
+      '<img class="eventtia-card-expand_img" src="' + obj_data.widget_image + '">\n' +
+      '</div>\n' +
+      '<div class="eventtia-card-expand_content">\n' +
+      '<p class="eventtia-card-expand_badge">\n' +
+        obj_event_status["event_status"] + '\n' +
+      '</p>\n' +
+      '<h5 class="eventtia-card-expand_date">\n' +
+        obj_dates["readable_date"] + '\n' +
+      '</h5>\n' +
+      '<h2 class="eventtia-card-expand_title">\n' +
+        obj_data.widget_title_text + '\n' +
+      '</h2>\n' +
+      '<p class="eventtia-card-expand_text">\n' +
+        obj_data.widget_description_text + '\n' +
+      '</p>\n' +
+      '<div class="eventtia-card-expand_button">\n' +
+      '<button class="eventtia-card-expand_btn eventtia-btn" eventtia-id="' + obj_data.id + '">\n' +
+        obj_data.widget_button_text + '\n' +
+      '</button>\n' +
+      '</div>\n' +
+      '</div>\n' +
+      '</div>\n' +
+      '</li>\n' +
+      '</ul>\n';
+    }
+    editCardClasses(card, "eventtia-card-expand_badge", "eventtia-card-expand_btn", obj_event_status);
+    
+  } else if (obj_data.widget_type === "compact") {
+    let card = document.getElementById(obj_data.id);
 
-      // console.log("Update expand card", event_status);
-      document.getElementsByClassName("eventtia-card-expand_badge")[0].innerHTML = "" + event_status_obj["event_status"] + "\n";
-
-      // update badge color
-      document.getElementsByClassName("eventtia-card-expand_badge")[0].classList.remove(event_status_obj["status_class_remove"]);
-      document.getElementsByClassName("eventtia-card-expand_badge")[0].classList.add(event_status_obj["status_class_add"]);
-
-      // update button color
-      document.getElementsByClassName("eventtia-card-expand_btn")[0].classList.remove(event_status_obj["status_class_remove"]);
-      document.getElementsByClassName("eventtia-card-expand_btn")[0].classList.add(event_status_obj["status_class_add"]);
-
-    } else if (events_data[event_key].widget_type === "compact") {
-      let card = document.getElementById(event_key);
-
+    if (first_call) {
       // create div card compact
       card.setAttribute('style', 'display: inline;');
       card.innerHTML =
@@ -268,63 +267,72 @@ function updateLayoutFromData(data) {
         '<li class="eventtia-cards-compact_item">\n' +
         '<div class="eventtia-card-compact">\n' +
         '<div class="eventtia-card-compact_image">\n' +
-        '<img class="eventtia-card-compact_img" src="' + events_data[event_key].widget_image + '">\n' +
+        '<img class="eventtia-card-compact_img" src="' + obj_data.widget_image + '">\n' +
         '</div>\n' +
         '<div class="eventtia-card-compact_content">\n' +
         '<p class="eventtia-card-compact_badge">\n' +
-          event_status_obj["event_status"] + '\n' +
+          obj_event_status["event_status"] + '\n' +
         '</p>\n' +
         '<h2 class="eventtia-card-compact_title">\n' +
-          events_data[event_key].widget_title_text + '\n' +
+          obj_data.widget_title_text + '\n' +
         '</h2>\n' +
         '<div class="eventtia-card-compact_button">\n' +
-        '<button class="eventtia-card-compact_btn eventtia-btn" eventtia-id="' + event_key + '">\n' +
-          events_data[event_key].widget_button_text + '\n' +
+        '<button class="eventtia-card-compact_btn eventtia-btn" eventtia-id="' + obj_data.id + '">\n' +
+          obj_data.widget_button_text + '\n' +
         '</button>\n' +
         '</div>\n' +
         '</div>\n' +
         '</div>\n' +
         '</li>\n' +
         '</ul>\n';
+    }
+    editCardClasses(card, "eventtia-card-compact_badge", "eventtia-card-compact_btn", obj_event_status);
 
-      // console.log("Update compact card", event_status);
-      document.getElementsByClassName("eventtia-card-compact_badge")[0].innerHTML = "" + event_status_obj["event_status"] + "\n";
+  } else if (obj_data.widget_type === "button") {
+    let card = document.getElementById(obj_data.id);
 
-      // update badge color
-      document.getElementsByClassName("eventtia-card-compact_badge")[0].classList.remove(event_status_obj["status_class_remove"]);
-      document.getElementsByClassName("eventtia-card-compact_badge")[0].classList.add(event_status_obj["status_class_add"]);
-
-      // update button color
-      document.getElementsByClassName("eventtia-card-compact_btn")[0].classList.remove(event_status_obj["status_class_remove"]);
-      document.getElementsByClassName("eventtia-card-compact_btn")[0].classList.add(event_status_obj["status_class_add"]);
-
-    } else if (events_data[event_key].widget_type === "button") {
-      let card = document.getElementById(event_key);
+    if (first_call) {
       // create div card compact
       card.setAttribute('style', 'display: inline;');
       card.innerHTML = 
-        '<button class="eventtia-card-button_btn eventtia-btn" eventtia-id="' + event_key + '">' + 
-          events_data[event_key].widget_button_text +
+        '<button class="eventtia-card-button_btn eventtia-btn" eventtia-id="' + obj_data.id + '">' + 
+          obj_data.widget_button_text + '\n' +
         '</button>';
-    } else {
-      console.log("Unrecognized widget type");
-    }
+    }    
+  } else {
+    console.log("Unrecognized widget type");
   }
+}
+
+function generalLayoutUpdate(id_eventtia, data_db, first_call) {
+  // get specific event data
+  let element_data = data_db["events_data"][id_eventtia];
+  let element_dates = data_db["parsed_dates"][id_eventtia];
+
+  // Find if event Scheduled, Live, Rediffusion
+  let event_status_obj = check_event_status(element_dates.event_time_finished, element_dates.event_time_user_adjusted, id_eventtia, element_data.widget_type, first_call);
+  updateLayoutFromData(element_data, element_dates, event_status_obj, first_call);
+  return [event_status_obj, element_dates, element_data];
 }
 
 let interaction_id_js = 0
 // main analytics function
-function update_session(type, interaction_id, url, status, event_date) {
+function update_session(type, interaction_id, data_db, id_eventtia) {
   if (db_id != "none") {
+    let checker_response = generalLayoutUpdate(id_eventtia, data_db, false);
+    let event_status_obj = checker_response[0];
+    let element_dates = checker_response[1];
+    let element_data = checker_response[2];
+
     (async function () {
       const { data, error } = await supabase
         .from('sessions')
         .insert([{
           session_id: db_id,
-          url: url,
+          url: element_data.url,
           action: type,
-          status: status,
-          event_date: event_date,
+          status: event_status_obj.status_class_add,
+          event_date: element_dates.original_event_date,
           interaction_id: interaction_id
         }], { returning: 'minimal' })
 
@@ -334,12 +342,12 @@ function update_session(type, interaction_id, url, status, event_date) {
         } else {
           if (type == 'close'){
             // stop updating session
-            clearInterval(interval);
+            clearInterval(intervals_obj[id_eventtia]);
           }
 
           if (type == 'open') {
             // start updating session every minute
-            interval = setInterval(function(){ update_session("update", interaction_id_js, url, status, event_date); }, 60000);
+            intervals_obj[id_eventtia] = setInterval(function(){ update_session("update", interaction_id_js, data_db, id_eventtia); }, 60000);
           }   
         } 
     })()
@@ -394,50 +402,40 @@ Promise.all(container_promises)
   .then(function (result) {
     // organize db data
     let db_data = organizeDataFromSupabase(result)
+    let status_interval = {};
 
     // modify DOM
-    updateLayoutFromData(db_data);
+    for (let i = 0; i < event_ids.length; i++) {
+      let eventtia_id = event_ids[i].id;
+      //update event status
+      generalLayoutUpdate(eventtia_id, db_data, true);
+      status_interval[eventtia_id] = setInterval(function () { generalLayoutUpdate(eventtia_id, db_data, false); }, 60000);
+    }
 
-    // modify DOM every minute
-    let status_interval = setInterval(function () { check_event_status(); }, 60000);
-
-    let eventtia_id = null;
-    let element_data = null;
-    let element_dates = null;
     for (let i = 0; i < btn.length; i++) {
       btn[i].onclick = function () {
-        eventtia_id = btn[i].getAttribute("eventtia-id");
-        element_data = db_data["events_data"][eventtia_id];
-        element_dates = db_data["parsed_dates"][eventtia_id];
+        let eventtia_id = btn[i].getAttribute("eventtia-id");
 
         // Show Modal
-        document.getElementById('embed-' + eventtia_id).src = element_data.url;
+        document.getElementById('embed-' + eventtia_id).src = db_data["events_data"][eventtia_id]["url"]; //only needs to be done the first time.
         document.getElementById('modal-' + eventtia_id).style.display = "block";
         interaction_id_js = Date.now();
 
-        // Find if event Scheduled, Live, Rediffusion
-        let event_status_obj = check_event_status(element_dates.event_time_finished, element_dates.event_time_user_adjusted);
-
         // save obj
-        update_session("open", interaction_id_js, element_data.url, event_status_obj.status_class_add, element_dates.original_event_date);
+        update_session("open", interaction_id_js, db_data, eventtia_id);
       }
     }
 
     for (let i = 0; i < spans.length; i++) {
       spans[i].setAttribute("style", "float: right; margin: 10px; font-weight: bold; cursor: pointer;");
       spans[i].onclick = function () {
-        eventtia_id = btn[i].getAttribute("eventtia-id");
-        element_data = db_data["events_data"][eventtia_id];
-        element_dates = db_data["parsed_dates"][eventtia_id];
+        let eventtia_id = btn[i].getAttribute("eventtia-id");
 
         // Hide modal
         document.getElementById('modal-' + eventtia_id).style.display = "none";
         
-        // Find if event Scheduled, Live, Rediffusion
-        let event_status_obj = check_event_status(element_dates.event_time_finished, element_dates.event_time_user_adjusted);
-
         // save obj
-        update_session("close", interaction_id_js, element_data.url, event_status_obj.status_class_add, element_dates.original_event_date);
+        update_session("close", interaction_id_js, db_data, eventtia_id);
       }
     }
   })
